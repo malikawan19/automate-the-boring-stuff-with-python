953ed0b45917373ff1d171b33f53d1dacfd596fa
malikawan19 <malikawan19@yahoo.com>
2017-05-16 13:01:00 +0500

    Comma Code program - version 1

---
+++ chapter-4/commacode1.py
@@ -0,0 +1,24 @@
+# Project Description:
+
+# Say you have a list value like this:
+# spam = ['apples', 'bananas', 'tofu', 'cats']
+# Write a function that takes a list value as an argument and returns
+# a string with all the items separated by a comma and a space, with and
+# inserted before the last item. For example, passing the previous spam list to
+# the function would return 'apples, bananas, tofu, and cats'. But your function
+# should be able to work with any list value passed to it.
+
+
+#Please check commacode2.py for a more concise version of this program
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+def comma(list):
+    result = ''
+    for i in range(len(list)):
+        if i != len(list) - 1:    #Not the last item in the list
+            result += list[i] + ", "
+        else:
+            result += 'and ' + list[i]
+    return result
+
+print(comma(spam))
